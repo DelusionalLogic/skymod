be432ddf93bb678f975f5684d1f07b22c55b25c0
Jesper Jensen <jesper@slashwin.dk>
2017-07-04 21:38:12 +0200

    Pep8-ify gitrepo file, and remove unsused import
    I forgot to remove progressbar import. Now we actually work on a bare
    install

--- skymod/repository/gitrepo.py
+++ skymod/repository/gitrepo.py
@@ -1,8 +1,7 @@
 from .packagerepo import PackageRepo
 import git
-import progressbar
 from tqdm import tqdm
-import time
+
 
 class TqdmUpTo(tqdm):
     def __init__(self, **kwargs):
@@ -19,12 +18,17 @@ class TqdmUpTo(tqdm):
             self.n = cur_count
         self.update(cur_count - self.n)
 
+
 class GitRemotePackageRepo(PackageRepo):
     def __init__(self, root, remote):
         if not root.exists() or not (root/".git").exists():
             print("Cloning repo")
             with TqdmUpTo(miniters=1) as bar:
-                self.repo = git.Repo.clone_from(remote, root, progress = bar.update_to)
+                self.repo = git.Repo.clone_from(
+                    remote,
+                    root,
+                    progress=bar.update_t
+                )
         else:
             self.repo = git.Repo(root)
         self.remote = self.repo.remote()
@@ -32,4 +36,4 @@ class GitRemotePackageRepo(PackageRepo):
 
     def update(self):
         with TqdmUpTo(miniters=1, total=100) as bar:
-            self.remote.pull(progress = bar.update_to)
+            self.remote.pull(progress=bar.update_to)

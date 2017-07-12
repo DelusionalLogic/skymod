3c4795c3197a221f85f65e918e858014b36e38d4
Jesper Jensen <jesper@slashwin.dk>
2017-07-12 19:30:48 +0200

    Add the license header to every python source file

--- skymod/repository/localrepo.py
+++ skymod/repository/localrepo.py
@@ -1,3 +1,19 @@
+# Copyright 2017 Jesper Jensen (delusionallogic)
+#
+# This file is part of skymod.
+#
+# skymod is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# skymod is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with skymod.  If not, see <http://www.gnu.org/licenses/>.
 from .packagerepo import PackageRepo
 from .errors import AlreadyInstalledError
 import skymod.package
